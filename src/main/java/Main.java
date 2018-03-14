467dbe2a56f54cb811e9932b6f3440d982171610
maciejszwaczka@gmail.com <maciejszwaczka@gmail.com>
2018-03-14 14:21:51 +0100

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,28 @@ public class Main {
     public static void main(String[] args)
     {
         System.setProperty("file.encoding","UTF-8");
-        Parser parser=new Parser();
-        List<Product> mensWear=parser.parse("https://www.citysport.com.pl/kategoria/odziez/bluzy-meskie/");
-        mensWear.addAll(parser.parse("https://www.citysport.com.pl/kategoria/odziez/koszulki-meskie/"));
-        mensWear.addAll(parser.parse("https://www.citysport.com.pl/kategoria/odziez/dresy-meskie/"));
+        List<Product> mensWear=Parser.parse("https://www.citysport.com.pl/kategoria/odziez/bluzy-meskie/");
+        mensWear.addAll(Parser.parse("https://www.citysport.com.pl/kategoria/odziez/koszulki-meskie/"));
+        mensWear.addAll(Parser.parse("https://www.citysport.com.pl/kategoria/odziez/dresy-meskie/"));
         XLSWriter.writeToXLS(mensWear,"Odziez meska");
 
 
-        List<Product> childsWear=parser.parse("https://www.citysport.com.pl/kategoria/odziez/bluzy-dzieciece/");
-        childsWear.addAll(parser.parse("https://www.citysport.com.pl/kategoria/odziez/dresy-dzieciece/"));
-        childsWear.addAll(parser.parse("https://www.citysport.com.pl/kategoria/odziez/koszulki-dzieciece/"));
+        List<Product> childsWear=Parser.parse("https://www.citysport.com.pl/kategoria/odziez/bluzy-dzieciece/");
+        childsWear.addAll(Parser.parse("https://www.citysport.com.pl/kategoria/odziez/dresy-dzieciece/"));
+        childsWear.addAll(Parser.parse("https://www.citysport.com.pl/kategoria/odziez/koszulki-dzieciece/"));
         XLSWriter.writeToXLS(childsWear,"Odziez dziecieca");
 
-        List<Product> mensBoots=parser.parse("https://www.citysport.com.pl/kategoria/buty/buty-meskie/");
+        List<Product> mensBoots=Parser.parse("https://www.citysport.com.pl/kategoria/buty/buty-meskie/");
         XLSWriter.writeToXLS(mensBoots,"Buty meskie");
 
-        List<Product> womensBoots= parser.parse("https://www.citysport.com.pl/kategoria/buty/buty-damskie/");
+        List<Product> womensBoots= Parser.parse("https://www.citysport.com.pl/kategoria/buty/buty-damskie/");
         XLSWriter.writeToXLS(womensBoots, "Buty damskie");
 
-        List<Product> childsBoots=parser.parse("https://www.citysport.com.pl/kategoria/buty/buty-dzieciece/");
+        List<Product> childsBoots=Parser.parse("https://www.citysport.com.pl/kategoria/buty/buty-dzieciece/");
         XLSWriter.writeToXLS(childsBoots,"Buty dzieciece");
 
-        List<Product> womensWear=parser.parse("https://www.citysport.com.pl/kategoria/odziez/bluzy-damskie/");
-        womensWear.addAll(parser.parse("https://www.citysport.com.pl/kategoria/odziez/koszulki-damskie/"));
+        List<Product> womensWear=Parser.parse("https://www.citysport.com.pl/kategoria/odziez/bluzy-damskie/");
+        womensWear.addAll(Parser.parse("https://www.citysport.com.pl/kategoria/odziez/koszulki-damskie/"));
         XLSWriter.writeToXLS(womensWear,"Odziez damska");
     }
 }
